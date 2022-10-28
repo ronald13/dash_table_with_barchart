d617a6541d94e64f7bac6a6e7e79ef0044c96222
Aliaksandr Ihnatsiuk <a.ihnatsiuk@digital-science.com>
2022-10-28 15:09:27 +0300

    Add theme switcher

--- app.py
+++ app.py
@@ -5,25 +5,38 @@ import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 from charts import create_table, set_layout
 import plotly.graph_objects as go
+from dash_bootstrap_templates import ThemeSwitchAIO
 from styling import PAGE_SIZE
+
+
+# select the Bootstrap stylesheet2 and figure template2 for the theme toggle here:
+template_theme1 = "lumen"
+template_theme2 = "darkly"
+url_theme1 = dbc.themes.LUMEN
+url_theme2 = dbc.themes.DARKLY
+
+
 
 # read data
 university = pd.read_csv('data/data.csv')
 
 app = Dash(
-    __name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
+    __name__, external_stylesheets=[url_theme1, dbc.themes.BOOTSTRAP],
 )
 server = app.server
 
 
 @app.callback(
-    Output('university_barchart', 'figure'),
-    [Input('university-table', "data"),
-     Input('university-table', "page_current"),
-     Input('university-table', "page_size")],
+    Output('barchart', 'figure'),
+
+    [Input('table', "data"),
+     Input('table', "page_current"),
+     Input('table', "page_size"),
+     Input(ThemeSwitchAIO.ids.switch("theme"), "value")],
     # [State("university-table", "columns")],
 )
-def update_table(data, page_current, page_size):
+def update_table(data, page_current, page_size, toggle):
+    template = template_theme1 if toggle else template_theme2
     df = pd.DataFrame(data)
     # sort df with pages
     df = df.iloc[page_current * page_size:(page_current + 1) * page_size]
@@ -39,17 +52,14 @@ def update_table(data, page_current, page_size):
         )
     )
     set_layout(row_barchart, heightgraph=len(df) * 42)
+    row_barchart.update_layout(template=template)
 
     return row_barchart
 
-
+theme_switch = html.Div(ThemeSwitchAIO(aio_id="theme", themes=[url_theme1, url_theme2] ), className='theme__switcher')
 header = html.Div([
-    html.Div([
         html.H1('Table + Bar'),
         html.P('There is no way to add a bar chart to the table in dash. You can work with Displaying Data Bars, but it looks rather clumsy'),
-    ], className='table2x'),
-
-
 ], className='dash__header')
 
 footer = html.Div([
@@ -59,14 +69,15 @@ footer = html.Div([
 
 app.layout = html.Div([
                     header,
+                    theme_switch,
                     html.Div([
                         html.Div([
                             html.Div('Table - Points information ', className="table_name"),
                             html.Div([
-                                    create_table(university, tableid='university-table', page_size=PAGE_SIZE, link_column='Player'),
+                                    create_table(university, tableid='table', page_size=PAGE_SIZE, link_column='Player'),
                                     html.Div(
                                         [
-                                            dcc.Graph(id='university_barchart', config={'displayModeBar': False})], style={'position': 'absolute', 'right': 0, 'top':'42px'}
+                                            dcc.Graph(id='barchart', config={'displayModeBar': False})], style={'position': 'absolute', 'right': 0, 'top':'42px'}
                                     ),
                                 ], style={'display': 'flex', 'position': 'relative', 'width': '100%'})
                         ]),
